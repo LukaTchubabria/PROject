f8e1f55cc851f191fd797706cebc0d24816eadec
LukaTchuba <lukachubabria@gmail.com>
2021-03-02 05:21:09 +0400

    9

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-//Js 
+    //..resposcive menu ...//
 var btn = document.getElementById("menu-btn");
 var menu = document.getElementById("menu-bg");
 btn.addEventListener("click" , function(){
@@ -8,4 +8,14 @@ btn.addEventListener("click" , function(){
 var back = document.getElementById("back") ;
 back.addEventListener("click" , function(){
     menu.style.left = "-100%" ;
+});
+
+    //..delete class...//
+
+var a = document.querySelectorAll(".img-anim");
+
+window.addEventListener("resize", function () {
+    for (var i = 0; i < a.length; i++) {
+        if (window.innerWidth < 769) a[i].classList.remove("animation-div");
+    }
 });
